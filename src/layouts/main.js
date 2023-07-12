8e313c7c724eabb02ce670eab2686b40e7818e8e
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-12 11:05:43 +0600

    packages installed

---
+++ src/layouts/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Main = () => {
+    return (
+        <div>
+            Header
+            <Outlet/>
+            Footer
+        </div>
+    )
+}
+
+export default Main
