a85367dfdd69b01c0fc879bf53ba4c7f04e45b6e
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-07-18 17:35:19 +0600

    homepage done

---
+++ src/components/footer/footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer py-5 bg-black">
+      <div className="container">
+        <div className="text-center">
+          <p className="text-white mb-4">
+            Copyrights © 2023 DIGI-CRD. All Rights Reserved.
+          </p>
+
+          <ul className="flex justify-center items-center gap-5 text-white">
+            <li>
+                <a href="/" className="text-white">
+                Privacy Policy
+                </a>
+            </li>
+            |
+            <li>
+                <a href="/" className="text-white">
+                Privacy Policy Terms and Conditions
+                </a>
+            </li>
+            |
+            <li>
+                <a href="/" className="text-white">
+               Disclaimer
+                </a>
+            </li>
+            |
+            <li>
+                <a href="/" className="text-white">
+                Refund Policy
+                </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/header'
+import Footer from '../components/footer/footer'
 
 const Main = () => {
     return (
         <div>
             <Header/>
             <Outlet/>
-            Footer
+            <Footer/>
         </div>
     )
 }
